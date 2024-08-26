fe6c850321b30ace1334649a80f04c1a398d9128
yokose-jin <yokose.jin.413@s.kyushu-u.ac.jp>
2024-08-26 23:37:16 +0900

    add images

--- app/about/contacts/page.tsx
+++ app/about/contacts/page.tsx
@@ -3,25 +3,26 @@ import styles from "./contacts.module.css"
 import BaseLayout from '@/app/_components/layouts/BaseLayout'
 import GitHubIcon from "@mui/icons-material/GitHub"
 import Link from 'next/link'
+import Image from "next/image";
 
 const contactsPage = () => {
   return (
     <BaseLayout title="Contacts">
         <main className={styles.main}>
           <Link href={"s"} className={styles.link} target='_blank'>
-            <GitHubIcon className={styles.icon}/>
+            <Image src='/img/icons/zenn_icon.svg' alt="" width={100} height={100}/>
             <div className='text-cyan-500'>
               zenn
             </div>
           </Link>
           <Link href={"s"} className={styles.link} target='_blank'>
-            <GitHubIcon className={styles.icon}/>
+          <Image src='/img/icons/qiita_icon.png' alt="" width={100} height={100}/>
             <div className='text-lime-400'>
               Qiita
             </div>
           </Link>
           <Link href={"s"} className={styles.link} target='_blank'>
-            <GitHubIcon className={styles.icon}/>
+          <Image src='/img/icons/note_icon.svg' alt="" width={100} height={100}/>
             <div>
               note
             </div>
